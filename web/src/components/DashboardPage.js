814a8dc7ae75cd86bdf4de266b17f9e135ab5aa6
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-04-22 15:37:57 -0700

    css(tailwind

--- web/src/components/DashboardPage.js
+++ web/src/components/DashboardPage.js
@@ -3,15 +3,14 @@ import { ReactComponent as Circle } from "../assets/circle.svg";
 
 const statuses = {
   ok: "text-green-500",
-  pending: "text-yellow-400",
-  symptoms: "text-red-700",
+  pending: "text-slate-400",
+  symptoms: "text-red-500",
 };
 
 const generateEmployees = () => {
   const employees = [];
   for (let i = 0; i < 10; i++) {
     const name = faker.name.fullName();
-
     employees.push({
       name: name,
       email: `${name.split(" ").join(".").toLowerCase()}@gmail.com`,
@@ -25,7 +24,7 @@ const generateEmployees = () => {
 
 export default function DashboardPage(props) {
   return (
-    <div className="px-10">
+    <div className="px-10 m-24">
       <div className="flex items-center justify-between w-full mb-20">
         <div className="text-5xl font-medium">Dashboard</div>
         {/* <div className="text-sm underline">Edit campaign</div> */}
@@ -39,17 +38,17 @@ function EmployeeList(props) {
   const employees = generateEmployees();
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-0 m-10">
       <div className="flex justify-between items-center mb-6">
-        <div className="basis-1/4 grow-0 font-bold text-xl">
-          Employees - <span>{employees.length}</span>
+        <div className="basis-1/4 grow-0 font-semibold text-xl">
+          Employees - <span className="font-thin">{employees.length}</span>
         </div>
         <div className="basis-1/4 grow-0 text-sm">Email</div>
         <div className="basis-1/4 grow-0 text-sm">Phone</div>
         <div className="basis-1/12 grow-0 text-sm">Status</div>
       </div>
       {employees.map((e) => (
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between py-7 rounded-2xl transition-all cursor-pointer hover:bg-slate-200">
           <div className="basis-1/4 grow-0">
             <div className="flex gap-3 items-center font-bold">
               <Circle className="h-10" />
@@ -58,9 +57,7 @@ function EmployeeList(props) {
           </div>
           <div className="basis-1/4 grow-0 text-gray-500">{e.email}</div>
           <div className="basis-1/4 grow-0 text-gray-500">{e.phone}</div>
-          <div className={"basis-1/12 grow-0 " + statuses[e.status]}>
-            {e.status}
-          </div>
+          <div className={"basis-1/12 grow-0 " + statuses[e.status]}>{e.status}</div>
         </div>
       ))}
     </div>
--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { ReactComponent as Circle } from "../assets/circle.svg";
 
 export default function Navbar(props) {
   return (
-    <div className="m-5">
+    <div className="mx-5 my-9">
       <div className="h-20 flex items-center relative">
         <Logo className="h-16" />
         <div className="w-10 text-sm ml-1 text-black font-medium">
