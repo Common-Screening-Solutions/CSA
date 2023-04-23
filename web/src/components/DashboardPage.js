4335cfc5d9e91430969e5780b17552cb76768836
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-04-23 09:35:37 -0700

    scrollbars

--- web/src/components/DashboardPage.js
+++ web/src/components/DashboardPage.js
@@ -22,13 +22,19 @@ const generateEmployees = () => {
   return employees;
 };
 
-
-
 export default function DashboardPage(props) {
   return (
     <div className="mx-36">
       <div className="text-5xl font-medium mb-20">Dashboard</div>
-      <EmployeeList/>
+      <div className="flex justify-between items-center mb-6 px-3 mx-10 pr-[54px]">
+        <div className="basis-1/4 grow-0 font-semibold text-xl">
+          Employees - <span className="font-thin">10</span>
+        </div>
+        <div className="basis-1/4 grow-0 text-sm">Email</div>
+        <div className="basis-1/4 grow-0 text-sm">Phone</div>
+        <div className="basis-1/12 grow-0 text-sm">Status</div>
+      </div>
+      <EmployeeList />
     </div>
   );
 }
@@ -43,26 +49,24 @@ function EmployeeList(props) {
   // }
 
   return (
-    <div className="flex flex-col gap-0 m-10">
-      <div className="flex justify-between items-center mb-6 px-3">
-        <div className="basis-1/4 grow-0 font-semibold text-xl">
-          Employees - <span className="font-thin">{employees.length}</span>
-        </div>
-        <div className="basis-1/4 grow-0 text-sm">Email</div>
-        <div className="basis-1/4 grow-0 text-sm">Phone</div>
-        <div className="basis-1/12 grow-0 text-sm">Status</div>
-      </div>
+    <div className="flex flex-col gap-0 m-10 max-h-[46vh] overflow-y-scroll overflow-x-hidden">
       {employees.map((e) => (
-        <div className="flex items-center justify-between py-7 px-3 rounded-2xl transition-all hover:bg-slate-200">
+        <div className="flex items-center justify-between py-7 px-3 pr-12 rounded-l-2xl transition-all hover:bg-slate-100">
           <div className="basis-1/4 grow-0 select-none">
             <div className="flex gap-3 items-center font-bold">
               <Circle className="h-10" />
               {e.name}
             </div>
           </div>
-          <div className="basis-1/4 grow-0 text-gray-500 select-none">{e.email}</div>
-          <div className="basis-1/4 grow-0 text-gray-500 select-none">{e.phone}</div>
-          <div className={"basis-1/12 grow-0 select-none " + statuses[e.status]}>
+          <div className="basis-1/4 grow-0 text-gray-500 select-none">
+            {e.email}
+          </div>
+          <div className="basis-1/4 grow-0 text-gray-500 select-none">
+            {e.phone}
+          </div>
+          <div
+            className={"basis-1/12 grow-0 select-none " + statuses[e.status]}
+          >
             {e.status}
           </div>
         </div>
