c269f93692f81f43dbd560484edfb7032de5c167
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2023-04-23 14:09:45 -0700

    form page stuff

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,9 +1,14 @@
 import { ReactComponent as Right } from "../assets/right.svg";
 
-export default function Button({ text, onClick, arrow = false }) {
+export default function Button({
+  text,
+  onClick,
+  arrow = false,
+  height = "h-10",
+}) {
   return (
     <div
-      className="h-10 flex items-center justify-center gap-2 bg-gray-900 text-white rounded-lg shadow-lg select-none cursor-pointer"
+      className={`${height} flex items-center justify-center gap-2 bg-gray-900 text-white rounded-lg shadow-lg select-none cursor-pointer`}
       onClick={onClick}
     >
       {text}
--- web/src/components/FormPage.js
+++ web/src/components/FormPage.js
@@ -1,4 +1,6 @@
 import Button from "./Button.js";
+import InputField from "./InputField.js";
+import { useState } from "react";
 
 const symptoms = [
   "Coughing",
@@ -10,22 +12,52 @@ const symptoms = [
 ];
 
 export default function FormPage(props) {
+  const [code, setCode] = useState("");
+  const [auth, setAuth] = useState(false);
+
+  function submit() {}
+
+  function tryAuth() {
+    setAuth(true);
+  }
+
   return (
     <div className="m-5">
       <div className="text-4xl font-medium">Screening Form</div>
       <div className="mt-4 bg-slate-300 h-px w-screen absolute left-0"></div>
-      <div className="mt-16 flex flex-col items-center">
-        <div className="text-3xl font-bold mb-8">COVID-19</div>
-        <div className="text-md font-medium text-center mb-10">
-          Have you experienced any of the following symptoms in the past 7 days?
-        </div>
-        {symptoms.map((s) => (
-          <div className="flex justify-between w-full font-medium text-xl mb-6">
-            {s}
-            <input type="checkbox" />
+      {auth ? (
+        <div className="mt-16 flex flex-col items-center">
+          <div className="text-3xl font-bold mb-8">COVID-19</div>
+          <div className="text-md font-medium text-center mb-10">
+            Have you experienced any of the following symptoms in the past 7
+            days?
           </div>
-        ))}
-      </div>
+          {symptoms.map((s) => (
+            <div className="flex justify-between w-full font-medium text-xl mb-6">
+              {s}
+              <input className="w-6" type="checkbox" />
+            </div>
+          ))}
+          <div className="w-3/4 absolute bottom-8">
+            <Button text="Submit" onClick={submit} height="h-16" />
+          </div>
+        </div>
+      ) : (
+        <div className="mt-16 flex flex-col items-center">
+          <div className="w-32">
+            <InputField
+              onInput={(e) => {
+                setCode(e.target.value);
+              }}
+              placeholder="4-digit code"
+              maxlength={4}
+            />
+          </div>
+          <div className="w-3/4 absolute bottom-8">
+            <Button text="Next" onClick={tryAuth} arrow={true} height="h-16" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
