2ba92d9eff2c7d4d18ba4c741e5f816f1a54d18a
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-04-22 17:51:49 -0700

    juicy css

--- web/src/components/DashboardPage.js
+++ web/src/components/DashboardPage.js
@@ -22,11 +22,13 @@ const generateEmployees = () => {
   return employees;
 };
 
+
+
 export default function DashboardPage(props) {
   return (
     <div className="mx-36">
       <div className="text-5xl font-medium mb-20">Dashboard</div>
-      <EmployeeList />
+      <EmployeeList/>
     </div>
   );
 }
@@ -45,7 +47,7 @@ function EmployeeList(props) {
         <div className="basis-1/12 grow-0 text-sm">Status</div>
       </div>
       {employees.map((e) => (
-        <div className="flex items-center justify-between py-7 rounded-2xl transition-all cursor-pointer hover:bg-slate-200">
+        <div className="flex items-center justify-between py-7 px-3 rounded-2xl transition-all cursor-pointer hover:bg-slate-200 hover:scale-105">
           <div className="basis-1/4 grow-0">
             <div className="flex gap-3 items-center font-bold">
               <Circle className="h-10" />
